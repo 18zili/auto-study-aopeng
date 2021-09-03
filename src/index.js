aaf62479fef4067347edf92edb6b3075de75a7f9
十八子李 <94770222@qq.com>
2021-09-03 10:37:56 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,39 +2,19 @@ require('colors');
 
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const findChrome = require('carlo/lib/find_chrome');
 
 const config = require('./config');
 const message = require('./message');
 
 puppeteer.use(StealthPlugin());
 
-const log = console.log
-console.log = (msg) => {
-    message(msg)
-    log(msg)
-}
-
 try {
     (async () => {
-        console.log('正在查找 Chrome...'.green);
-        const findChromePath = await findChrome({});
-        const executablePath = findChromePath.executablePath;
-        let browser
-        if (findChromePath && executablePath) {
-            console.log('正在下载 Chrome...'.green);
-            browser = await require('puppeteer-extra').launch({
-                executablePath,
-                headless: true
-            });
-        } else {
-            browser = await require('puppeteer').launch({
-                executablePath,
-                headless: true
-            });
-        }
-        console.log('正在打开 Chrome...'.green);
-        console.log('正在打开奥鹏学习平台...'.green);
+        message('正在打开 Chrome...');
+        let browser = await require('puppeteer').launch({
+            headless: true
+        });
+        message('正在打开奥鹏学习平台...');
         const page = await browser.newPage();
         await page.goto(config.PATH, {
             waitUntil: [
@@ -43,7 +23,7 @@ try {
             ]
         });
     
-        console.log('正在登录...'.green);
+        message('正在登录...');
         const usernameEl = await page.$('#username');
         await usernameEl.type(config.USERNAME, { delay: 40 });
         const passwordEl = await page.$('#pwd');
@@ -51,18 +31,18 @@ try {
         const loginBtn = await page.$('#loginbtn');
         await loginBtn.click();
     
-        console.log('登录成功 正在跳转...'.green);
+        message('登录成功 正在跳转...');
         await page.waitForNavigation();
     
-        console.log('正在获取课程列表...'.green);
+        message('正在获取课程列表...');
         await page.waitForSelector('#onlinework-list .show_box');
         const onlineworkNames = await page.$$eval('#onlinework-list .show_box .show_bottom_left .show_bottom_first',el => el.map(el => el.innerHTML));
         const onlineworkLinks = await page.$$eval('#onlinework-list .show_box .show_bottom_right a',el => el.map(el => el.href));
-        console.log(`获取到 ${ onlineworkNames.length } 门课程.`.green);
+        message(`获取到 ${ onlineworkNames.length } 门课程.`);
     
-        console.log('开始签到...'.green);
+        message('开始签到...');
         for (let i = 0; i < onlineworkLinks.length; i++) {
-            console.log(`正在签到: ${ onlineworkNames[i] }...`.green);
+            message(`正在签到: ${ onlineworkNames[i] }...`);
             const link = onlineworkLinks[i];
             const workPage = await browser.newPage();
             await workPage.goto(link, {
@@ -75,7 +55,7 @@ try {
             });
             await workPage.close();
         }
-        console.log('签到完成.'.green);
+        message('签到完成.');
     })();
 } catch (error) {
     message(error)
